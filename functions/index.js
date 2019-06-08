26c2548529ac0d9fb3e3534255831b9bd860b261
Roneet Kumar <roneetparida@gmail.com>
2019-06-07 22:48:37 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,18 @@
 $(function() {
-
-    $('svg.back-btn').on('click', function() {
+    $('svg.back-btn').click(function() {
         $('.input-page-wrapper').show();
         $('.weather-data-wrapper,.app-bar').hide();
         $('input.search').val('');
         $('.card').remove();
     });
-    $('button.sub-btn').on('click', function() {
+
+    $('input.search').keypress(function(e) {
+        if (e.which == 13) {
+            $('button.sub-btn').click();
+        }
+    });
+
+    $('button.sub-btn').click(function() {
         if ($('input.search').val() != '') {
             var city = $('input.search').val();
             var key = '895ff6619f3b61beff40fae1c36905d4';
@@ -25,7 +31,6 @@ $(function() {
                         $('.weather-data-wrapper').append(
                             '<div class="card"><div class="date-time"><h1>Date : ' + data.list[i].dt_txt.split(" ", 1) + '</h1><h1>Time : ' + data.list[i].dt_txt.split(" ", 2)[1] + '</h1><h1>Temp : ' + data.list[i].main.temp + ' C</h1><h1>Min : ' + data.list[i].main.temp_min + ' C</h1><h1>Max : ' + data.list[i].main.temp_max + ' C</h1><h1>Pressure : ' + data.list[i].main.pressure + '</h1><h1>Sea Level : ' + data.list[i].main.sea_level + '</h1><h1>Ground Level : ' + data.list[i].main.grnd_level + '</h1><h1>Humidity : ' + data.list[i].main.humidity + '</h1><h1>Wind Speed : ' + data.list[i].wind.speed + '</h1><h1>Wind Deg : ' + data.list[i].wind.deg + '</h1><h1>Clouds : ' + data.list[i].clouds.all + '%</h1></div></div>');
                     }
-
                     $('.input-page-wrapper').hide();
                     $('.weather-data-wrapper,.app-bar').show();
                 }
@@ -59,6 +64,7 @@ $(function() {
                 url: weather,
                 dataType: "jsonp",
                 success: function(data) {
+                    $('.card').remove();
                     $('button.find-location svg').removeClass('btnClicked');
                     $('.city-name').html(data.city.name + ", " + data.city.country);
                     for (var i = 0; i < data.list.length; i++) {
